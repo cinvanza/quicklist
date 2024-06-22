4804cb7f4c88bb9e830f9a3bed6b3f69f54e694d
luis colmenares <luiscolme101@gmail.com>
2024-06-22 17:49:23 -0300

    edit y add para listas

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :products
-  resources :lists
+  resources :lists do
+    resources :products, only: [:new, :create, :edit, :update]
+  end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
