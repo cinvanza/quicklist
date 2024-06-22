329472600293a2c1a5f5038771b13ffc691ef365
vanesa camano <vanesasoledad90@gmail.com>
2024-06-22 16:57:34 +0100

    fixing error

--- app/models/list_guest.rb
+++ app/models/list_guest.rb
@@ -1,11 +1,4 @@
 class ListGuest < ApplicationRecord
   belongs_to :list
   belongs_to :user
-  validates :first_name, presence: true, length: { minimum: 2, maximum: 50 }
-  validates :last_name, presence: true, length: { minimum: 2, maximum: 50 }
-  validates :country, presence: true, length: { minimum: 2, maximum: 50 }
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true, length: { minimum: 8 }
-  validates :password_confirmation, presence: true
-  validate :password_complexity
 end
