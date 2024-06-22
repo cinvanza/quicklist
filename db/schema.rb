4a48c4bebfd8a19587d7e9780ddb44da2b09ee08
cintia vanesa zavi <cintia_zavi@hotmail.com>
2024-06-22 13:19:54 -0300

    migraciones hechas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_22_144045) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_22_154733) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_22_144045) do
     t.index ["supermarket_id"], name: "index_lists_on_supermarket_id"
     t.index ["tag_id"], name: "index_lists_on_tag_id"
     t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "brand"
+    t.decimal "price"
+    t.integer "quantity"
+    t.bigint "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_products_on_list_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -71,6 +82,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_22_144045) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "country"
+    t.string "currency"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -80,6 +95,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_22_144045) do
   add_foreign_key "lists", "supermarkets"
   add_foreign_key "lists", "tags"
   add_foreign_key "lists", "users"
+  add_foreign_key "products", "lists"
   add_foreign_key "reviews", "lists"
   add_foreign_key "reviews", "users"
 end
