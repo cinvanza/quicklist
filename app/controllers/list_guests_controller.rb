c2226225cb6c0ab8e4a4a7da64eb2bd4fed73d63
vanesa camano <vanesasoledad90@gmail.com>
2024-06-25 23:30:04 +0100

    form search guest

--- app/controllers/list_guests_controller.rb
+++ app/controllers/list_guests_controller.rb
@@ -1,18 +1,29 @@
 class ListGuestsController < ApplicationController
+  def new
+    @list_guest = ListGuest.new
+  end
+
   def create
-    @user = User.find_by(email: "jose@email.com")
+    user = User.find_by(email: params[:email])
 
-    if @user
-      @list_guest = ListGuest.create(user_id: @user.id, email: @user.email)
+    if user
+      @list_guest = ListGuest.new(list_guest_params)
+      @list_guest.user = user
 
-      if @list_guest.persisted?
-        render json: { message: 'Guest successfully added to the list' }, status: :created
-        
+      if @list_guest.save
+        redirect_to some_path, notice: 'Guest was successfully created.'
       else
-        render json: { errors: @list_guest.errors.full_messages }, status: :unprocessable_entity
+        render :new
       end
     else
-      render json: { error: 'User not found. Please enter another email.' }, status: :not_found
+      flash[:alert] = 'User not found'
+      render :new
     end
+  end
+
+  private
+
+  def list_guest_params
+    params.require(:list_guest).permit(:list_id, :user_id)
   end
 end
