da7d488e15ab5fbbc32098626f2a7116d0bc7c62
cintia vanesa zavi <cintia_zavi@hotmail.com>
2024-06-27 20:07:35 -0300

    avances en home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,5 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @lists = [
-      { title: "Cena Navideña", tags: ["Navidad", "Cena", "Festivo"], path: "/lists/cena_navidena" },
-      { title: "Fiesta", tags: ["Fiesta", "Celebración", "Entretenimiento"], path: "/lists/fiesta" },
-      { title: "Velorio", tags: ["Funeral", "Condolencias"], path: "/lists/velorio" },
-      { title: "Cumpleaños", tags: ["Cumpleaños", "Celebración", "Fiesta"], path: "/lists/cumpleanos" }
-    ]
   end
 end
