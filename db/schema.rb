8588abeef509ae0bdad20e900db96cfaf3b6523d
Jose Cerrato <jfcerrato92@gmail.com>
2024-07-16 21:47:41 -0600

    some changes

--- db/schema.rb
+++ db/schema.rb
@@ -42,12 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_14_193606) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "chatrooms", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "list_guests", force: :cascade do |t|
     t.bigint "list_id", null: false
     t.bigint "user_id", null: false
@@ -72,16 +66,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_14_193606) do
     t.index ["supermarket_id"], name: "index_lists_on_supermarket_id"
     t.index ["tag_id"], name: "index_lists_on_tag_id"
     t.index ["user_id"], name: "index_lists_on_user_id"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.string "content"
-    t.bigint "chatroom_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -121,15 +105,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_14_193606) do
     t.string "image"
   end
 
-  create_table "user_lists", force: :cascade do |t|
-    t.bigint "chatroom_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["chatroom_id"], name: "index_user_lists_on_chatroom_id"
-    t.index ["user_id"], name: "index_user_lists_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -152,9 +127,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_14_193606) do
   add_foreign_key "lists", "supermarkets"
   add_foreign_key "lists", "tags"
   add_foreign_key "lists", "users"
-  add_foreign_key "messages", "chatrooms"
-  add_foreign_key "messages", "users"
   add_foreign_key "reviews", "users"
-  add_foreign_key "user_lists", "chatrooms"
-  add_foreign_key "user_lists", "users"
 end
